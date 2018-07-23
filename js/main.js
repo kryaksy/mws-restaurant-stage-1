510bcc45ed233f77e23082fb2aa6e5365884055c
Ibrahim Aksoy <ib.koray@gmail.com>
2018-07-23 05:19:44 +0300

    Added new theme order (Initialize)
    - updated index and restaurant HTML
    - updated CSS
    - temporarily deleted map info

--- js/main.js
+++ js/main.js
@@ -80,9 +80,9 @@ initMap = () => {
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
     mapboxToken: 'pk.eyJ1Ijoia3J5YWtzeSIsImEiOiJjamp4N24zZWYzNzB6M3dsZWY1MWk3OTZ2In0.Q7r9D0TSKyq1nKLKYAfRxg',
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    //   '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    //   'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
   }).addTo(newMap);
 
